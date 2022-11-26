4989fd7fd8b1226565e14c3c1da984bd38ab8467
Anjana Bandara <57900949+Anjanamb@users.noreply.github.com>
2022-11-27 02:00:38 +0530

    Added random input generation for 3bit counter

--- Counter_3bit_AsyDown/test_counter_3bit_AsyDown.vhd
+++ Counter_3bit_AsyDown/test_counter_3bit_AsyDown.vhd
@@ -4,6 +4,8 @@
 LIBRARY IEEE;
 LIBRARY MAXII;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.MATH_REAL.uniform;
+USE IEEE.MATH_REAL.floor;
 USE MAXII.MAXII_COMPONENTS.ALL;
 
 -- Set up this testbench as an entity
@@ -13,19 +15,15 @@ end test_counter_3bit_AsyDown;
 -- Create an implementation of the entity
 -- (May have several per entity)
 architecture testbench1 of test_counter_3bit_AsyDown is
-
-  -- Set up the signals on the 3bit_counter_AsyDown
   signal button1 : std_logic;
   signal button4 : std_logic;
   signal led1    : std_logic;
   signal led2    : std_logic;
   signal led3    : std_logic;
-
   -- Set up the vcc signal as 1
   signal vcc  : std_logic := '1';
-  
-  begin
-    -- dut = device under test (same name as top project from Quartus)
+begin
+  -- dut = device under test (same name as top project from Quartus)
     dut : entity work.counter_3bit_AsyDown
       -- Map the ports from the dut to this testbench
       port map (
@@ -35,13 +33,26 @@ architecture testbench1 of test_counter_3bit_AsyDown is
         led2 => led2,
         led3 => led3 );
     
-    -- Set up the signals    
-    stimulus : process is
-      begin
-        -- Just make a clock on button1 to simulate pushing the button
-        loop
-          button1 <= '0'; wait for 10 ns;
-          button1 <= '1'; wait for 10 ns;
-        end loop;
-      end process stimulus;
-end architecture testbench1;
+  -- Set up the signals    
+  stimulus : process is
+    variable seed1 : positive;
+    variable seed2 : positive;
+    variable x : real;
+    variable y : integer;
+  begin
+    seed1 := 999;
+    seed2 := 999;
+    loop
+      uniform(seed1, seed2, x);
+      y := integer(x);
+      report "Number is " & integer'image(y);
+      if (y=1) then
+            button1 <= '1'; wait for 1 ps;
+	    report "button1 is 1";
+      else
+            button1 <= '0'; wait for 1 ps;
+	    report "button1 is 0";
+      end if;
+    end loop;
+  end process;
+end architecture;
