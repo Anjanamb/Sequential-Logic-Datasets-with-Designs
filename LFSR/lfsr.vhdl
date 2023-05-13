dfad99c743f072f91ad9d28e3aca8048553e958b
Sahan Nayanajith <sahantm0@gmail.com>
2023-05-13 15:12:59 +0530

    Update lfsr.vhdl

--- LFSR/lfsr.vhdl
+++ LFSR/lfsr.vhdl
@@ -1,54 +1,27 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-USE STD.TEXTIO.ALL;
 
-ENTITY Testbench IS
-END Testbench;
+ENTITY LFSR8 IS
+  PORT (Clk, Rst: IN std_logic;
+        output: OUT std_logic_vector (7 DOWNTO 0));
+END LFSR8;
 
-ARCHITECTURE TBarch OF Testbench IS
-  COMPONENT LFSR8 IS
-    PORT (Clk, Rst: IN std_logic;
-          output: OUT std_logic_vector (7 DOWNTO 0));
-  END COMPONENT;
-
-  SIGNAL Clk_s, Rst_s: std_logic;
-  SIGNAL output_s: std_logic_vector(7 DOWNTO 0);
-
-file tb_file : text open write_mode is "tb_output.txt";
-
+ARCHITECTURE LFSR8_beh OF LFSR8 IS
+  SIGNAL Currstate, Nextstate: std_logic_vector (7 DOWNTO 0);
+  SIGNAL feedback: std_logic;
 BEGIN
-  CompToTest: LFSR8 PORT MAP (Clk_s, Rst_s, output_s);
 
-  write_to_file : process(Clk_s)
-  variable line : line;
-  begin
-    if rising_edge(Clk_s) then
-	  write(line, std_logic'image(Rst_s));
-	  write(line, ' ');
-          write(line, std_logic'image(output_s(0)));
-	  write(line, ' '); -- Separate with a space
-	  writeline(tb_file, line);
-    end if;
-  end process write_to_file;
-  Clk_proc: PROCESS
+  StateReg: PROCESS (Clk,Rst)
   BEGIN
-    Clk_s <= '1';
-    WAIT FOR 10 ns;
-    Clk_s <= '0';
-    WAIT FOR 10 ns;
-  END PROCESS clk_proc;
-                      
-  Vector_proc: PROCESS
-  BEGIN
-    Rst_s <= '1';
-    WAIT FOR 5 NS;
-    Rst_s <= '0';
-    FOR index IN 0 To 4 LOOP
-      WAIT UNTIL Clk_s='1' AND Clk_s'EVENT;
-    END LOOP;
-    WAIT FOR 5 NS;
-    ASSERT output_s = X"88" REPORT "Failed output=88";
-    WAIT;
-  END PROCESS Vector_proc;
+    IF (Rst = '1') THEN
+      Currstate <= (0 => '1', OTHERS =>'0');
+    ELSIF (Clk = '1' AND Clk'EVENT) THEN
+      Currstate <= Nextstate;
+    END IF;
+  END PROCESS;
+  
+  feedback <= Currstate(4) XOR Currstate(3) XOR Currstate(2) XOR Currstate(0);
+  Nextstate <= feedback & Currstate(7 DOWNTO 1);
+  output <= Currstate;
 
-END TBarch;
+END LFSR8_beh;
